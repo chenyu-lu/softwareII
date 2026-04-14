17f75fe5eac8c562b509ce20a778e11d5562bc34
qiaoqikang <3393457251@qq.com>
2026-04-14 19:53:48 +0800

    springboot+maven框架建设

---
+++ src/main/java/com/example/softwareii/SoftwareIiApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwareii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareIiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareIiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/softwareii/SoftwareIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softwareii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareIiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
